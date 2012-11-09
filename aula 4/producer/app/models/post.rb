3b39280d93fd31867e0b0763d5515f1ec67999ac
AndersonFurlane <andersonfurlanemartins@gmail.com>
2012-11-09 15:11:57 -0200

    Aula 7 part 1

--- aula 4/producer/app/models/post.rb
+++ aula 4/producer/app/models/post.rb
@@ -1,10 +1,15 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :excerpt, :title, :author_id, :category_ids
+
+  attr_accessible :body, :excerpt, :title, :author_id, :category_ids,
+  :slug, :draft
   #nao permite valores nil nos objetos na hora de criar ou alterar um post
-  validates_presence_of :title, :body, :author
+
+  validates_presence_of :title, :body, :author, :slug
   validates_associated :author
+  validates_inclusion_of :draft, in: [true, false]
 
   has_many :comments, :dependent => :destroy
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_and_belongs_to_many :categories
+
 end
